25365eb196edd4dfff410bb59273be129c32bfec
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-12 18:26:17 +0530

    added SideNav component

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,11 +1,24 @@
 <template>
   <TopNav />
+  <div 
+    :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+    class="flex justify-between mx-auto w-full lg:px-2.5 px-0"
+  >
+    <div>
+      <SideNavMain />
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
   setup() {
+    const route = useRoute()
     
+    return {
+      route
+    }
   },
 }
 </script>
