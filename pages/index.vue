904844609dd3e2065a6afd2306e3240a069eec63
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-12 13:13:57 +0530

    testing basic nuxt dev environment (npm run dev)

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-red-500">
+    Testing
+    <Icon name="mdi:close" size="100" />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    
+  },
+}
+</script>
