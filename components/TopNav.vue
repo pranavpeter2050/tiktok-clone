91e2b1e42542fe905a8a0a8b7c89b2c717a0b112
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-12 13:26:36 +0530

    created "components" folder

---
+++ components/TopNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div 
+    id="TopNav" 
+    class="fixed bg-white z-30 flex items-center w-full border-b h-[61px]"
+  >
+    <div 
+      :class="route.fullPath === '/' ? 'max-w-[1150px]' : ''"
+      class="flex items-center justify-between w-full px-6 mx-auto"
+    >
+      <div :class="route.fullPath === '/' ? 'w-[80%]' : 'lg:w-[20%] w-[70%]'">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const route = useRoute()
+
+    return {
+      route
+    }
+  },
+}
+</script>
