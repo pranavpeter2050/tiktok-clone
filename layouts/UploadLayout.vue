15eff92eb6f0ece20af8d6be52ddf8f2ba8f1b12
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-13 00:39:39 +0530

    added design of Upload page (UploadLayout, upload/index.vue)

---
+++ layouts/UploadLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div 
+  class="bg-[#f8f8f8] h-[100vh]"
+  >
+    <TopNav />
+    <div class="flex justify-between mx-auto w-full px-2 max-w-[1140px]">
+      <slot />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const route = useRoute()
+    
+    return {
+      route
+    }
+  },
+}
+</script>
