7d780fb7dd9771e622f6452a12007c1e3be06d46
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-14 23:55:40 +0530

    added modal for "register"

---
+++ components/Register.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="text-center text-[28px] mb-4 font-bold">Sign up</div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Full name"
+      v-model:input="name"
+      inputType="text"
+      :autofocus="true"
+      error=""
+    />
+  </div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Email address"
+      v-model:input="email"
+      inputType="email"
+    />
+  </div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Password"
+      v-model:input="password"
+      inputType="password"
+    />
+  </div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Confirm password"
+      v-model:input="confirmPassword"
+      inputType="password"
+    />
+  </div>
+
+  <div class="px-6 text-[12px] text-gray-600">Forgot password?</div>
+
+  <div class="px-6 pb-2 mt-6">
+    <button 
+      :disabled="(!name || !email || !password || !confirmPassword)"
+      :class="(!name || !email || !password || !confirmPassword) ? 'bg-gray-200' : 'bg-[#f02c56]'"
+      @click="register"
+      class="w-full text- font-semibold text-white py-3 rounded-sm"
+    >
+      Sign up
+    </button>
+  </div>
+</template>
+
+<script setup>
+let name = ref(null)
+let email = ref(null)
+let password = ref(null)
+let confirmPassword = ref(null)
+let errors = ref(null)
+</script>
