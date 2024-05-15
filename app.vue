77028ee9ff4299c0b4fb9e80490dd6be0f98485f
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-15 14:27:36 +0530

    using "plugin/stores" to show the login-modal

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <NuxtPage />
 
-  <AuthOverlay />
+  <AuthOverlay v-if="isLoginOpen"/>
 </template>
+
+<script setuo>
+import { storeToRefs } from 'pinia';
+
+const { $generalStore } = useNuxtApp()
+
+const { isLoginOpen } = storeToRefs($generalStore)
+</script>
