c826b1bafacac5a4bc768b20a00b716fcf03d31b
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-15 14:27:42 +0530

    created "plugin" to add auth, baseURL to axios

---
+++ plugins/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export default defineNuxtPlugin((NuxtApp) => {
+  
+  axios.defaults.withCredentials = true
+  axios.defaults.baseURL = 'http://localhost:3000'
+  return {
+    provide: {
+      axios: axios
+    }
+  }
+})
